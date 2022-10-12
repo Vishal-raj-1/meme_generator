5e83cbdabf1f14c7ecbb426e8fd94a687211f316
Vishal Rajput <rajputvishal33786@gmail.com>
2022-10-13 01:07:50 +0530

    Adding save text button for mobile device

--- src/components/AddText.jsx
+++ src/components/AddText.jsx
@@ -10,10 +10,10 @@ const AddText = () => {
             {
                 editMode
                     ?
-                    <div className='flex items-center justify-around'>
+                    <>
                         <textarea value={text} onChange={(e) => setText(e.target.value)}className='py-2 pl-3 font-bold my-2 text-primary w-100' />
-                        <button onClick={() => setEditMode(false)} className='rounded-md bg-indigo-600 py-1 my-5 px-4 font-semibold text-white hover:bg-indigo-500'>Save Text</button>
-                    </div>
+                        <button onClick={() => setEditMode(false)} className='rounded-md bg-indigo-600 py-1 my-5 px-4 font-semibold text-white hover:bg-indigo-500 md:hidden'>Save Text</button>
+                    </>
                     : <p className='text-3xl font-bold font-helvetica text-primary' onDoubleClick={() => setEditMode(true)}>{text}</p>
             }
         </Draggable>
