a476eaa148ccd4f525b3bc99251360687e6548ef
Vishal Rajput <rajputvishal33786@gmail.com>
2022-10-13 01:25:21 +0530

    fix

--- src/components/AddText.jsx
+++ src/components/AddText.jsx
@@ -11,7 +11,7 @@ const AddText = () => {
                 editMode
                     ?
                     <>
-                        <textarea value={text} onChange={(e) => setText(e.target.value)}className='py-2 pl-3 font-bold my-2 text-primary w-100' />
+                        <textarea value={text} onChange={(e) => setText(e.target.value)} className='py-2 pl-3 font-bold my-2 text-primary w-100' onDoubleClick={() => setEditMode(false)} />
                         <button onClick={() => setEditMode(false)} className='rounded-md bg-indigo-600 py-1 my-5 px-4 font-semibold text-white hover:bg-indigo-500 md:hidden'>Save Text</button>
                     </>
                     : <p className='text-3xl font-bold font-helvetica text-primary' onDoubleClick={() => setEditMode(true)}>{text}</p>
